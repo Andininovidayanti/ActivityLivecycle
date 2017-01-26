790e98d24717b668529163c39af65524e99926ea
Andininovidayanti <andininovi09@gmail.com>
2017-01-25 23:52:40 -0800

    Livecycle Music

--- app/src/main/java/com/example/andininovidayantia/activitylivecycle/MainActivity.java
+++ app/src/main/java/com/example/andininovidayantia/activitylivecycle/MainActivity.java
@@ -1,5 +1,6 @@
-package id.sch.smktelkom_mlg.learn.ActivityLivecycle;
+package id.sch.smktelkom_mlg.learn.activitylivecycle;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -9,13 +10,70 @@ import com.example.andininovidayantia.activitylivecycle.R;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "LivecycleTag";
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);package id.sch.smktelkom_mlg.learn.activitylivecycle;
+
+        import android.media.MediaPlayer;
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.util.Log;
+
+        public class MainActivity extends AppCompatActivity {
+
+            private static final String TAG = "LivecycleTag";
+            MediaPlayer mediaPlayer;
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                Log.d(TAG, "onCreate: ");
+
+                mediaPlayer = MediaPlayer.create(this, R.raw.song);
+            }
+
+            @Override
+            protected void onStart() {
+                super.onStart();
+                Log.d(TAG, "onStart: ");
+            }
+
+            @Override
+            protected void onResume() {
+                super.onResume();
+                mediaPlayer.start();
+                Log.d(TAG, "onResume: ");
+            }
+
+            @Override
+            protected void onPause() {
+                super.onPause();
+                mediaPlayer.pause();
+                Log.d(TAG, "onPause: ");
+            }
+
+            @Override
+            protected void onStop() {
+                super.onStop();
+                Log.d(TAG, "onStop: ");
+            }
+
+            @Override
+            protected void onDestroy() {
+                super.onDestroy();
+                if (mediaPlayer != null) mediaPlayer.release();
+                Log.d(TAG, "onDestroy: ");
+            }
+        }
         setContentView(R.layout.activity_main);
 
         Log.d(TAG, "onCreate: ");
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song);
     }
 
     @Override
@@ -27,12 +85,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+        mediaPlayer.start();
         Log.d(TAG, "onResume: ");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+        mediaPlayer.pause();
         Log.d(TAG, "onPause: ");
     }
 
@@ -45,6 +105,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if (mediaPlayer != null) mediaPlayer.release();
         Log.d(TAG, "onDestroy: ");
     }
 }
